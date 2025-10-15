05e2dfad4b8961d14d94ed95561219b38d898868
680710713 <thongsawat_p@silpakorn.edu>
2025-10-15 04:51:46 +0000

    add code.c

---
+++ hello.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    printf("Simple Calculator\n");
+    printf("Enter expression (e.g., 5 + 3): ");
+    if (scanf("%lf %c %lf", &num1, &op, &num2) != 3) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: Division by zero.\n");
+            } else {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            }
+            break;
+        default:
+            printf("Unsupported operator.\n");
+    }
+
+    return 0;
+}
